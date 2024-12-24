72dece8b0782db2a6a8dbcbe7faaca3f97778c5e
SantoshPayasi <santoshpayasi78@gmail.com>
2024-12-25 02:38:41 +0530

    initial commit

---
+++ agileboard_backend/src/main/java/com/spdev/agileboard_backend/AgileboardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.spdev.agileboard_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileboardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileboardBackendApplication.class, args);
+	}
+
+}
---
+++ agileboard_backend/src/test/java/com/spdev/agileboard_backend/AgileboardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spdev.agileboard_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgileboardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
